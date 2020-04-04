0d8d48f5bd5bfa7addff4befdae0a96c2eda08bc
ktalmage <ktalmage85@gmail.com>
2020-04-04 12:30:49 -0400

    show, index page adjustments, restaurant_review_controller adjustments

--- review-house/app/controllers/restaurant_reviews_controller.rb
+++ review-house/app/controllers/restaurant_reviews_controller.rb
@@ -34,43 +34,56 @@ class RestaurantReviewsController < ApplicationController
     end
     
     get "/reviews/:id" do
-        
-     if logged_in?
+      if logged_in?
       @restaurant = RestaurantReview.find(params[:id])
       erb :"reviews/show"
      else
       redirect '/login'
-    end
-  end
-    
-    get "/reviews/:id/edit" do
-      @error_message = params[:error]
-      @restaurant = RestaurantReview.find(params[:id])
-      erb :"reviews/edit"
-    end
-    
-    patch "/review/:id" do
-      
-        if @restaurant.update(params)
-        redirect "/reviews/#{@restaurant.id}/edit"
-        else
-        
-        redirect "/reviews/#{@restaurant.id}"
-        end
-    end
-    
-    delete '/reviews/:id' do
-      find_review
-      if @restaurant.destroy
-        redirect '/reviews'
-      else
-        redirect "/reviews/#{@restaurant.id}"
       end
     end
-
+    
+    get "/reviews/:id/edit" do
+     if !logged_in?
+      redirect to '/login'
+     end
+     find_review
+     if @restaurant.user_id = current_user.id
+      erb :'/reviews/edit'
+     else
+      redirect to '/login'
+     end
+    end
+    
+    patch "/reviews/:id" do
+    
+      find_review
+      if params[:id].empty?
+        redirect "/reviews/#{@restaurant.id}/edit"
+      end
+        @restaurant.update(params)
+        @restaurant.save
+        redirect "/reviews/#{@restaurant.id}"
+      end
+    
+    
+    delete '/reviews/:id' do
+      if logged_in?
+        find_review
+      if @restaurant.user == current_user(session)
+        @restaurant = RestaurantReview.find_by_id(params[:id])
+        @restaurant.delete
+        redirect to '/reviews'
+      else
+        redirect to "/"
+      end
+      else
+        redirect to '/login'
+      end
+    end
+  
     private
 
     def find_review
       @restaurant = RestaurantReview.find_by_id(params[:id])
     end
-  end
+    end
